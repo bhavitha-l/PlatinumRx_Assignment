0bff9f8dda8e3fb17a27779cd9287b8e7f525850
bhavitha-l <bhavitha2k@gmail.com>
2026-04-08 21:07:57 +0530

    Refactor SQL queries and add comments

--- sql_solutions.sql
+++ sql_solutions.sql
@@ -18,6 +18,7 @@ WHERE MONTH(b.booking_date) = 11 AND YEAR(b.booking_date) = 2021
 GROUP BY bc.booking_id;
 
 
+//Bills > 1000 (Oct 2021)//
 SELECT bill_id,
        SUM(i.item_rate * bc.item_quantity) AS bill_amount
 FROM booking_commercials bc
@@ -27,7 +28,7 @@ GROUP BY bill_id
 HAVING SUM(i.item_rate * bc.item_quantity) > 1000;
 
 
-
+//Most & Least Ordered Item per Month (2021)//
 WITH item_orders AS (
     SELECT MONTH(bc.bill_date) AS month,
            bc.item_id,
@@ -45,7 +46,7 @@ ranked AS (
 SELECT * FROM ranked
 WHERE most_rank = 1 OR least_rank = 1;
 
-
+//Second Highest Bill per Month//
 WITH monthly_bills AS (
     SELECT MONTH(bc.bill_date) AS month,
            b.user_id,
@@ -65,14 +66,14 @@ ranked AS (
 SELECT * FROM ranked
 WHERE rnk = 2;
 
-
+//Revenue by Sales Channel//
 SELECT sales_channel,
        SUM(amount) AS revenue
 FROM clinic_sales
 WHERE YEAR(datetime) = 2021
 GROUP BY sales_channel;
 
-
+//Top 10 Customers//
 SELECT uid,
        SUM(amount) AS total_spent
 FROM clinic_sales
@@ -81,7 +82,7 @@ GROUP BY uid
 ORDER BY total_spent DESC
 LIMIT 10;
 
-
+//Month-wise Revenue, Expense, Profit//
 WITH revenue AS (
     SELECT MONTH(datetime) AS month,
            SUM(amount) AS total_revenue
@@ -107,7 +108,7 @@ SELECT r.month,
 FROM revenue r
 JOIN expenses_cte e ON r.month = e.month;
 
-
+//Most Profitable Clinic per City//
 WITH clinic_profit AS (
     SELECT c.city, c.cid,
            SUM(cs.amount) - COALESCE(SUM(e.amount),0) AS profit
@@ -124,7 +125,7 @@ ranked AS (
 )
 SELECT * FROM ranked WHERE rnk = 1;
 
-
+//Most Profitable Clinic per City//
 WITH clinic_profit AS (
     SELECT c.state, c.cid,
            SUM(cs.amount) - COALESCE(SUM(e.amount),0) AS profit
